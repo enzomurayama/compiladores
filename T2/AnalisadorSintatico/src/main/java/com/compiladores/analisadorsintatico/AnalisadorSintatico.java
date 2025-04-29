344e91f69c940d5daad72d0a1fe7c0d0e0e61cc1
Enzo Murayama <enzomurayama@gmail.com>
2025-04-29 19:00:38 -0300

    criação do projeto T2 - Analisador Sintático

---
+++ T2/AnalisadorSintatico/src/main/java/com/compiladores/analisadorsintatico/AnalisadorSintatico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.compiladores.analisadorsintatico;
+
+/**
+ *
+ * @author enzo_murayama
+ */
+public class AnalisadorSintatico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
